584fa733a41ff998cd4b9d661cdc1e6819cb0511
Tom Helmuth <thelmuth@hamilton.edu>
2026-09-25 15:27:21 +0000

    Code for testing that an exception is raised.

--- lazySortedList/test_lazySortedList.py
+++ lazySortedList/test_lazySortedList.py
@@ -3,7 +3,7 @@ test_lazySortedList.py
 """
 
 import pytest
-from lazySortedList import LazySortedList
+from lazySortedList import LazySortedList, UnsortedError
 
 def test_init_empty():
     lsl = LazySortedList()
@@ -22,3 +22,16 @@ def test_append_to_empty():
     assert lsl._list == [-44]
     assert not lsl._is_sorted
 
+
+def test_binary_search_exception():
+    """Note: We sometimes want to test things besides something being true.
+    Here, we test that an exception is thrown if trying to binary search
+    using an unsorted list."""
+    lsl = LazySortedList([8, 2, 5, 7])
+
+    ## Add this afterward, and see that test fails if list is sorted
+    # lsl.sort()
+
+    ## Says: code within this block should raise UnsortedError; if not, test fails
+    with pytest.raises(UnsortedError):
+        lsl.binary_search(5)
